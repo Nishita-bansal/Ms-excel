8ed2f4959b8eabff5a9aad64c36a4e3b86bea54c
Nishita Bansal <34601106+Nishita-bansal@users.noreply.github.com>
2020-12-07 12:17:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-// npm init -y
-// npm install electron--- isse electron install hojayega 
-const electron = require("electron");
+// create main.js file 
 // npm init -y
 // npm install electron
-// "start":"electron ."
+// add "start":"electron ."
 // npm install ejs-electron
 
-// electron logic
+const electron = require("electron");
+const ejse = require('ejs-electron');
 
 
-const {app , BrowserWindow} = electron;
-const ejse = require("ejs-electron");
 
-//const ejse = require("ejs-electron");
-function createWindow(){
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true, // desktop appplication usme node enable
-          //enableRemoteModule:true
-        }
-      })
-      win.loadFile('index.ejs').then(function(){
-      win.maximize();
-      win.webContents.openDevTools();
-      });
-    }
-app.whenReady().then(createWindow);
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true, // desktop application usme node enabled hojaega
+        enableRemoteModule:true
+      }
+    })
+    win.loadFile('index.ejs').then(function(){
+        win.maximize();
+        win.webContents.openDevTools() // you will get dev tools opened by default 
+    });
+  }
+
+
+
+app.whenReady().then(createWindow)
